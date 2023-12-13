711c46ea0b7e16ea88c4ce3f82d67747678e6711
JMADA257 <jadams1812@hotmail.com>
2023-12-12 18:20:08 -0700

    Just added pages and compenents folder to the app, researching react native currently

---
+++ client/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      This will be the home page, will be using react native currently looking
+      more into it.
+    </div>
+  );
+}
+
+export default HomePage;
